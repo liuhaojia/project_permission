3b017cbebd2e62c84ec7d03433e222e8668a72cb
JontyWang <501211312@qq.com>
2020-07-21 08:54:15 +0800

    update Home pircture
    更改首页大图

--- vue-permission/vue-cli3.0-vueadmin/src/pages/home/component/total-status.vue
+++ vue-permission/vue-cli3.0-vueadmin/src/pages/home/component/total-status.vue
@@ -15,7 +15,7 @@ export default {
     data() {
         return {
             src:
-                'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg'
+                'https://gitee.com/my_netinlove/Picturebed/raw/master/images/1595292629962.jpg'
         }
     }
 }
